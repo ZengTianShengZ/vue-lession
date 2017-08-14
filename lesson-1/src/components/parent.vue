108e39bedf3a31a02cdae11c55f6b36d3a308c28
zengtianshengz <zengtianshengz@163.com>
2017-08-14 20:12:35 +0800

    init poj

---
+++ lesson-1/src/components/parent.vue
@@ -0,0 +1,32 @@
+/**
+* @description: 文件或模块描述
+* @author: zengtiansheng
+* @update: 2017/8/14
+*/
+<style scoped>
+
+</style>
+<template>
+    <sun :message="msg"></sun>
+</template>
+<script>
+    import sun from './sun.vue'
+
+    export default {
+        data() {
+            return {
+                msg: 'something msg'
+            }
+        },
+        methods: {},
+        mounted() {
+
+        },
+        created() {
+
+        },
+        components: {
+            sun
+        }
+    }
+</script>
---
+++ lesson-1/src/components/sun.vue
@@ -0,0 +1,30 @@
+/**
+* @description: 文件或模块描述
+* @author: zengtiansheng
+* @update: 2017/8/14
+*/
+<style scoped>
+
+</style>
+<template>
+    <div>{{message}}</div>
+</template>
+<script>
+    export default {
+        props: {
+            message: ''
+        },
+        data() {
+            return {
+                msg: ''
+            }
+        },
+        methods: {},
+        mounted() {
+
+        },
+        created() {
+
+        }
+    }
+</script>
